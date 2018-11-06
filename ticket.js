5ea0af8b7eca9fb40e2eacd3968fde32d8b8f0f0
Sean McPherson <seanmcp25@gmail.com>
2018-11-06 09:58:58 -0500

    Render tickets

--- ticket.js
+++ ticket.js
@@ -1,22 +1,40 @@
 var newInput = document.getElementById('new_input');
 var newButton = document.getElementById('new_button');
+var ticketsList = document.getElementById('tickets_list');
 
 function addTicket() {
-    chrome.storage.sync.get('tickets', function(result) {
-        var list = result.tickets.slice(0);
-        var newTicket = {
-            id: new Date().getTime,
-            name: newInput.value
-        }
-        list.push(newTicket)
-        chrome.storage.sync.set({
-            ticket: list
-        }, function() {
-            alert('Success!');
-            newInput.value = '';
-            // setCount(newCount);
+    if (newInput.value) {
+        chrome.storage.sync.get('tickets', function(result) {
+            var list = result.tickets.slice(0);
+            var date = new Date();
+            var newTicket = {
+                id: date.getTime(),
+                name: newInput.value
+            }
+            list.push(newTicket)
+
+            chrome.storage.sync.set({
+                tickets: list
+            }, function() {
+                newInput.value = '';
+                renderTickets();
+            });
         });
+    }
+}
+
+function renderTickets() {
+    while (ticketsList.firstChild) {
+        ticketsList.removeChild(ticketsList.firstChild);
+    }
+    chrome.storage.sync.get('tickets', function(result) {
+        result.tickets.forEach(function(ticket) {
+            var item = document.createElement('li');
+            item.textContent = ticket.name;
+            ticketsList.appendChild(item);
+        })
     });
 }
 
 newButton.addEventListener('click', addTicket);
+renderTickets();
